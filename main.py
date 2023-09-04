41d263f6ecd1c62cecbf8e0777dd3f658be596cc
SLuk11 <121468000+SLuk11@users.noreply.github.com>
2023-09-04 13:30:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     config.read_config()
 
     log.logs_dir_check(config.shipping_xlsx['log_dir'])
-    log.write_log('customer_receive_process: process start')
+    log.write_log('[INFO] Start customer excel receiving process')
 
     ## directory part
 
@@ -33,6 +33,8 @@ if __name__ == '__main__':
 
             # Create batch running number per workbook
             batch_id = controllers.create_batch_id()
+            log.write_log('[INFO] Start converting file {} to SQL'.format(excel_file))
+            log.write_excel_log(excel_file, '[INFO] Start converting Excel to SQL process')
 
             process_status = []
             # loop to read and convert each sheet (in contained_sheets list) for each Excel file
@@ -48,14 +50,15 @@ if __name__ == '__main__':
                 # Check df count or length > 0
                 if len(df) > 0:
                     ## Clening data in data frame before convert to JSON format
-                    df_clean = controllers.df_clean(df, sheet[1]['sheet_name'])
+                    df_clean = controllers.df_clean(df, sheet[1]['sheet_name'], excel_file)
 
                     ## Execute to SQL database
                     if len(df_clean) > 0:
                         result = controllers.write2sql(config.LSP_Shipping_DB_conn_str, sheet[1]['to_table']
                                                        , df_clean, eval('config.{}'.format(sheet[1]['mapping_cols'])),
                                                        batch_id
-                                                       , eval('config.{}'.format(sheet[1]['default_value'])))
+                                                       , eval('config.{}'.format(sheet[1]['mars_default_value']))
+                                                       , excel_file)
                         # if complete write to SQL process
                         if result:
                             sheet_status = [sheet[1]['sheet_name'], result, ""]
@@ -87,31 +90,44 @@ if __name__ == '__main__':
                     # Get error message
                     err_sheet = process_status[status.index(False)][0]
                     err_msg = process_status[status.index(False)][2]
-
-                    # Collect fail batch transaction
-                    controllers.write2batch_table(config.LSP_Shipping_DB_conn_str, batch_id, excel_file
-                                                  , 0, err_msg='Sheet {} {}'.format(err_sheet, err_msg))
+                    log.write_log('[ERROR] Sheet {} {}'.format(err_sheet, err_msg))
 
                     # Delete all inserted row that contain batch_id which in fail process
                     controllers.del_sql_row_bybatch(config.LSP_Shipping_DB_conn_str
-                                                    , config.LSP_Shipping_DB['sql_table_list'], batch_id)
+                                                    , config.LSP_Shipping_DB['sql_table_list'], batch_id, excel_file)
+
+                    # Get encode log
+                    encode_text = controllers.encode_log(config.shipping_xlsx['log_dir'], excel_file)
+
+                    # Collect fail batch transaction
+                    controllers.write2batch_table(config.LSP_Shipping_DB_conn_str, batch_id, excel_file
+                                                  , 0, err_msg='Sheet {} {}'.format(err_sheet, err_msg)
+                                                  , compress_text= encode_text)
 
                     # move Excel file to fail folder after process do not success
                     controllers.move_file(excel_file, config.shipping_xlsx['source_dir']
                                           , config.shipping_xlsx['fail_dir'])
 
+
                 # if complete process every sheets
                 else:
                     # Collect success batch transaction
-                    controllers.write2batch_table(config.LSP_Shipping_DB_conn_str, batch_id, excel_file, 1)
+                    log.write_log('[INFO] Complete converting file {} to SQL'.format(excel_file))
+
+                    # Get encode log
+                    encode_text = controllers.encode_log(config.shipping_xlsx['log_dir'], excel_file)
+                    controllers.write2batch_table(config.LSP_Shipping_DB_conn_str, batch_id, excel_file, 1
+                                                  , compress_text= encode_text)
 
                     # Move Excel file to success folder after process success
                     controllers.move_file(excel_file, config.shipping_xlsx['source_dir']
                                           , config.shipping_xlsx['success_dir'])
+
+
     # Do not have any xlsx file in source directory
     else:
-        log.write_log('{} there have no .xlsx files in folder'.format(config.shipping_xlsx['source_dir']))
+        log.write_log('[INFO] There is no .xlsx files in {} folder'.format(config.shipping_xlsx['source_dir']))
 
     ## performance counter timing parts
     toc = time.perf_counter()
-    log.write_log(f'customer_receive_process: process end in {toc - tic:0.4f} seconds')
+    log.write_log(f'[INFO] Customer excel receiving process end in {toc - tic:0.4f} seconds')
