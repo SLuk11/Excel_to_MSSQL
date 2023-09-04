5d014b114ce0bf1d204060fa7012be2576afd3ae
SLuk11 <121468000+SLuk11@users.noreply.github.com>
2023-09-04 13:30:33 +0700

    Update __init__.py

--- logger/__init__.py
+++ logger/__init__.py
@@ -1,6 +1,7 @@
-from .logger import logs_dir_check, write_log
+from .logger import logs_dir_check, write_log, write_excel_log
 
 __all__ = [
     'logs_dir_check',
+    'write_excel_log',
     'write_log'
 ]
